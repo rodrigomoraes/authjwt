2a16baa4ec959ba0e45849398264995e0ade5ab3
rodrigo moraes <rodrigomoraes63@gmail.com>
2021-11-03 22:00:21 -0300

    Criaçao do model

--- app.js
+++ app.js
@@ -7,11 +7,30 @@ const jwt = require('jsonwebtoken');
 
 const app = express()
 
+
+// Configurar JSON response
+
+app.use(express.json())
+
+
 // Open Route - Rota Publica
 app.get('/', (req, res) => {
     res.status(200).json({msg: 'Hello World'})
 });
 
+// Registrar usuario
+app.post('/auth/register', async(req, res) =>{
+    const {name, email, password, confirmpassword} = req.body
+
+    // Validacoes
+    if(!name){
+        return res.status(422).json({msg: "Nome obrigatorio"})
+    }
+});
+
+
+
+// Credenciais
 const dbUser = process.env.DB_USER
 const dbPass = process.env.DB_PASS
 
